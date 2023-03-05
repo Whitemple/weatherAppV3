f4c80965beeea3742a0e71d775a12297b2e494eb
whitemple <yarikkhromey@mail.ru>
2023-03-05 12:36:01 +0300

    installed other helpful files. More info in description

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["jest"],
+  rules: {
+    "import/prefer-default-export": "off",
+    "max-len": [
+      "error",
+      {
+        ignoreComments: true,
+      },
+    ],
+    "import/extensions": [0, { js: "always" }],
+  },
+};
---
+++ src/example/example.test.js
@@ -0,0 +1,7 @@
+import { example } from "./example.js";
+
+describe("Example: ", () => {
+  it("Example: ", () => {
+    expect(example(1, 2)).toBe(3);
+  });
+});
