b2a9d695a037a9e825a9753a6f4990dd69b4fecf
whitemple <yarikkhromey@mail.ru>
2023-03-05 14:28:16 +0300

    create files for codding

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+// Объявлем переменные
+// const inputSearch = document.querySelector('.header__search');
+// const btnSearch = document.querySelector('.header__btn');
+// const btnGeolocation = document.querySelector('.header__geolocation');
+// const main__cardInfo = document.querySelector('.main__card-info');
+// const cityTitle = document.querySelector('.main__card-city');
+// const tempTitle = document.querySelector('.main__card-temp');
+// const imgTitle = document.querySelector('.main__card-img');
+
+// // Пишем ассинхронную функцию для отправки и полчения данных по API
+
+// const getCity = async function (){
+
+// }
